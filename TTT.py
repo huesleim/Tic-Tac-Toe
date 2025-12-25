d2617c7a513db464464c75eee59f379106f0a0dc
Hueslei <234298003+huesleim@users.noreply.github.com>
2025-12-25 10:12:38 -0300

    Fix whole game logic

--- TTT.py
+++ TTT.py
@@ -6,7 +6,7 @@ def get_name (message):
         if valid_input != "":
             return valid_input
         else:
-            print ("Please type in your name, pal :(")
+            print("Please type in your name.")
             
 def assign():
     while True:
@@ -16,7 +16,8 @@ def assign():
             cpu_sign = "O"
             return player_sign, cpu_sign
         elif player_sign.lower() == "o" or player_sign == "0":
-            print ("Nice! Let's get going, then!")
+            player_sign = "O"
+            print ("Nice! Let'sa get going, then!")
             cpu_sign = "X"
             return player_sign, cpu_sign
         else:
@@ -40,95 +41,69 @@ def player_move():
             break
         else:
             print("Please, input a valid, empty cell number between 1 and 9.")
-def cpu_move():
-    valid_play = random.randint(1, 9)
-    if board[valid_play-1] != " ":
-        cpu_move()
-    else: board[valid_play-1] = cpu_sign
+
+def cpu_move(board, cpu_sign):
+    print ("My turn now!")
+    empties = [i for i, v in enumerate(board) if v == " "]
+    if empties:
+        choice = random.choice(empties)
+        board[choice] = cpu_sign
+
 
 def check_win():
     winning_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for combo in winning_combinations:
         a, b, c = combo
-        if board[a] == board[b] == board[c] and board[a] != " ": return True
-        else: return False
-##            if board[a] == player_sign:
-##                print (name, "has won the game!")
-##                return player_sign
-##            else:
-##                print ("I won!")
-##                return cpu_sign
-##            print ("Want to play again?")
+        if board[a] == board[b] == board[c] and board[a] != " ": return board[a]
+    return None
 
 def play_again():
-    play_again = input("Want to play again? Type yes or no.\n")
-    if play_again.lower() == "yes" or play_again.lower() == "y": return True
-    elif play_again.lower() == "no" or play_again.lower() == "n": return False
+    while True:
+        play_again = input("Want to play again? Type yes or no.\n")
+        if play_again.lower() in ("yes", "y"): 
+            return True
+        if play_again.lower() in ("no", "n"): 
+            return False
+        print ("I don't understand!")
 
 def show_score():
     print ("Score right now is:\n",name,":", score[0], " x CPU:", score[1], sep="")
     
-##def resolve(winner):
-##    if winner == player_sign:
-##        score[0] += 1
-##        print ("You won! Congrats!")
-##        show_score()
-##        keep_playing = play_again()
-##        break
-##    elif winner == cpu_sign:
-##        score[1] += 1
-##        print ("Heh, I won!")
-##        show_score()
-##        keep_playing = play_again()
-##        break
+def resolve(winner):
+    if winner == player_sign:
+       score[0] += 1
+       print ("You won! Congrats!")
+    elif winner == cpu_sign:
+       score[1] += 1
+       print ("Heh, I won!")
+    show_score()
+    return
+
     
 name = get_name ("Please, tell me your name!\n")
 score = [0,0]
 board = [" "] * 9
 keep_playing = True
 player_sign, cpu_sign = assign()
-player_turn = True if player_sign == "x" else False
+player_turn = True if player_sign.lower() == "x" else False
 print ("Hello ", name, "! Let's get started, shall we?", sep="")
 print_board (board)
 
-while " " in board and play_again:
+while " " in board and keep_playing:
     if player_turn:
         player_move ()
         print_board (board)
         player_turn = not player_turn
-        if check_win():
-            score[0] += 1
-            print ("You won! Congrats!")
-            show_score()
-            keep_playing = play_again()
-            board = [" "] * 9
-            continue
-            
+
     else:
-        print ("My turn now!")
-        cpu_move ()
+        cpu_move (board, cpu_sign)
         print_board (board)
         player_turn = not player_turn
-        if check_win():
-            score[1] += 1
-            print ("Heh, I won!")
-            show_score()
-            keep_playing = play_again()
-            board = [" "] * 9
-            continue
-            
-
-
-    
-
-
-# Setting up variables
-
-
-
-   
-
-
-# Win condition
-
-
+        
+    if check_win():
+        resolve(check_win())
+        keep_playing = play_again()
+        if not keep_playing: break
+        board = [" "] * 9
+        player_turn = not player_turn
+        print_board(board)
